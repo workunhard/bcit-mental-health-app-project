0da6d99f54397d797a734aae6f5995321be26aa7
Bingting Zhong <bzhong6@my.bcit.ca>
2021-11-01 23:03:47 -0700

    Modified directory.html

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+var  firebaseConfig = {
+  apiKey: "AIzaSyAIuQkV7cgkjwhKHC06nLcKQICtfLhh9_o",
+  authDomain: "comp1800-bby26.firebaseapp.com",
+  projectId: "comp1800-bby26",
+  storageBucket: "comp1800-bby26.appspot.com",
+  messagingSenderId: "626264143236",
+  appId: "1:626264143236:web:ad885148d65675e988d9d1"
+  };
+  
+
+  const app = firebase.initializeApp(firebaseConfig);
