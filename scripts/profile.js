15f70e4ed4d39bcaab2df1e867f31de0163525ec
Jay Wang <jwang550@my.bcit.ca>
2021-11-18 14:57:48 -0800

    Sprinting Sprint #3

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,33 @@
 console.log("summary.js has been loaded.");
 
+function insertName() {
+        firebase.auth().onAuthStateChanged(user => {
+            // Check if user is signed in:
+            if (user) {                                                                 
+                // Do something for the current logged-in user here: 
+                console.log(user.uid);
+                //go to the correct user document by referencing to the user uid
+                currentUser = db.collection("users").doc(user.uid);
+                //get the document for current user.
+                currentUser.get()
+			          .then(userDoc => {
+                   var user_Name = userDoc.data().name;
+                   console.log(user_Name);
+
+                   $(".grid-item-profile-profileName-Replace").text(user_Name);                         //using jquery
+                })
+            } else {
+                // No user is signed in.
+            }
+        });
+    }
+insertName();
+
+// BootStrap Modal
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
